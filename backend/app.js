fab48269c028eebba3947381e02c8bb129effb08
unknown <wayz.fight1717@gmail.com>
2025-07-06 12:30:56 +0200

    renew

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,8 @@ app.use(session({
 }));
 
 // Parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
